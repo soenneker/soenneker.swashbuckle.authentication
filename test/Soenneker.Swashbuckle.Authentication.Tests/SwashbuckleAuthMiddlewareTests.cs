a3b3e5f2b21eb7cb280384bde5610e83937880a4
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:43 -0500

    TUnit conversion

--- test/Soenneker.Swashbuckle.Authentication.Tests/SwashbuckleAuthMiddlewareTests.cs
+++ test/Soenneker.Swashbuckle.Authentication.Tests/SwashbuckleAuthMiddlewareTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Swashbuckle.Authentication.Tests;
 
-[Collection("Collection")]
-public class SwashbuckleAuthMiddlewareTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class SwashbuckleAuthMiddlewareTests : HostedUnitTest
 {
-    public SwashbuckleAuthMiddlewareTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public SwashbuckleAuthMiddlewareTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
